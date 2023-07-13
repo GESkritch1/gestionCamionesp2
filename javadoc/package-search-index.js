0add039d2e8d1ceeea4bfff2924a698c53a17b6d
GESkritch1 <92118749+GESkritch1@users.noreply.github.com>
2023-07-13 06:21:57 -0400

    Delete javadoc directory

--- javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Archivador"},{"l":"ContextoProblema"},{"l":"Guis"},{"l":"Main"},{"l":"UsosyMas"}];updateSearchResults();
